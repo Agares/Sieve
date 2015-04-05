9c98b869262f58e896d0560838ef4ffd1a5b6740
Agares <ja@agares.info>
2015-04-05 17:15:20 +0200

    Intristics!

--- Erastotenes/main.c
+++ Erastotenes/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <intrin.h>
 
 typedef unsigned long long bitmap_entry_t;
 #define NUMBERS_TO_CALCULATE (1024*1024*1024)
@@ -8,39 +9,39 @@ typedef unsigned long long bitmap_entry_t;
 
 inline void bitmap_set(bitmap_entry_t *bitmap, unsigned long long index)
 {
-	bitmap[index / BITMAP_ENTRY_BITS] |=  1LL << (index%BITMAP_ENTRY_BITS);
+	_bittestandset64(&bitmap[index / BITMAP_ENTRY_BITS], index%BITMAP_ENTRY_BITS);
 }
 
 inline void bitmap_unset(bitmap_entry_t *bitmap, unsigned long long index)
 {
-	bitmap[index / BITMAP_ENTRY_BITS] &= ~(1LL << (index%BITMAP_ENTRY_BITS));
+	_bittestandreset64(&bitmap[index / BITMAP_ENTRY_BITS], index%BITMAP_ENTRY_BITS);
 }
 
 inline bitmap_entry_t bitmap_get(bitmap_entry_t *bitmap, unsigned long long index)
 {
-	return bitmap[index / BITMAP_ENTRY_BITS] & (1LL << (index%BITMAP_ENTRY_BITS));
+	return _bittest64(&bitmap[index / BITMAP_ENTRY_BITS], index%BITMAP_ENTRY_BITS);
 }
 
-int main(void) 
+int main(void)
 {
-	bitmap_entry_t *bitmap = malloc((1 + NUMBERS_TO_CALCULATE/BITMAP_ENTRY_BITS) * sizeof(bitmap_entry_t));
+	bitmap_entry_t *bitmap = malloc((1 + NUMBERS_TO_CALCULATE / BITMAP_ENTRY_BITS) * sizeof(bitmap_entry_t));
 	for (int i = 0; i < (1 + NUMBERS_TO_CALCULATE / BITMAP_ENTRY_BITS); i++)
 	{
 		bitmap[i] = 0xFFFFFFFFFFFFFFFFLL;
 	}
 
-	for (int i = 2; i < sqrt(NUMBERS_TO_CALCULATE); i++) 
+	for (int i = 2; i < sqrt(NUMBERS_TO_CALCULATE); i++)
 	{
 		if (bitmap_get(bitmap, i - 2))
 		{
-			for (int j = 2 * i; j < NUMBERS_TO_CALCULATE; j+=i)
+			for (int j = 2 * i; j < NUMBERS_TO_CALCULATE; j += i)
 			{
 				bitmap_unset(bitmap, j - 2);
 			}
 		}
 	}
 
-	for (int i = 2; i < NUMBERS_TO_CALCULATE; i++) 
+	for (int i = 2; i < NUMBERS_TO_CALCULATE; i++)
 	{
 		if (bitmap_get(bitmap, i - 2))
 		{
